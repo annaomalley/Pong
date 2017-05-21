44d7eca47e5cbfa6dce0f48cc3bc4a64378cf66b
annaomalley <omalleya@seas.upenn.edu>
2017-05-21 17:54:37 +0200

    trying to draw a cup

--- app/src/main/java/mobile/pong/view/GameView.java
+++ app/src/main/java/mobile/pong/view/GameView.java
@@ -3,6 +3,10 @@ package mobile.pong.view;
 import android.view.View;
 import android.content.Context;
 import android.util.AttributeSet;
+import android.graphics.Paint;
+import android.graphics.Color;
+import android.graphics.Canvas;
+import android.util.Log;
 
 import mobile.pong.model.GameModel;
 
@@ -12,11 +16,25 @@ import mobile.pong.model.GameModel;
 
 public class GameView extends View{
 
+    private Paint paintCups;
+
     public GameView(Context context, AttributeSet attrs) {
         super(context, attrs);
+
+        paintCups = new Paint();
+        paintCups.setColor(Color.RED);
+        paintCups.setStyle(Paint.Style.STROKE);
+        paintCups.setStrokeWidth(5);
     }
 
-    private void drawCups() {
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        drawCups(canvas);
+
+    }
+
+
+    private void drawCups(Canvas canvas) {
         Boolean[] currentPlayerCups;
         Boolean[] opponentCups;
 
@@ -24,12 +42,26 @@ public class GameView extends View{
             opponentCups = GameModel.getInstance().getCups(2);
             currentPlayerCups = GameModel.getInstance().getCups(1);
         }
-        else if(GameModel.getInstance().getCurrentPlayer() == 2) {
+        else {
             opponentCups = GameModel.getInstance().getCups(1);
             currentPlayerCups = GameModel.getInstance().getCups(2);
         }
 
+        float radiusHorizontal = getWidth()/4;
+        float radiusVertical = getHeight()/3/4;
+        float radius = Math.min(radiusHorizontal,radiusVertical)/2;
+        //canvas.drawCircle(getWidth()/2,getHeight()/2, radius, paintCups);
 
+        drawBackRows(canvas,radius,currentPlayerCups,opponentCups);
+    }
+
+    private void drawBackRows(Canvas canvas, float radius, Boolean[] currentPlayerCups, Boolean[] opponentCups) {
+        Log.w("height/2", Double.toString((getHeight()/2)));
+        Log.w("radius", Double.toString((radius)));
+        float diameter = radius*2;
+
+
+        canvas.drawCircle(getWidth()/2+radius,diameter+radius,radius,paintCups);
     }
 
 
