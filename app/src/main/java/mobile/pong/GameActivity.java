00f782dbdcf075d1c4cd6b780d5cca9bae68f2e4
annaomalley <omalleya@seas.upenn.edu>
2017-05-21 16:12:18 +0200

    added GameView

--- app/src/main/java/mobile/pong/GameActivity.java
+++ app/src/main/java/mobile/pong/GameActivity.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import mobile.pong.view.GameView;
+
 public class GameActivity extends AppCompatActivity {
 
     @Override
@@ -18,14 +20,7 @@ public class GameActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        final GameView gameView = (GameView) findViewById(R.id.gameView);
     }
 
     @Override
---
+++ app/src/main/java/mobile/pong/view/GameView.java
@@ -0,0 +1,20 @@
+package mobile.pong.view;
+
+import android.view.View;
+import android.content.Context;
+import android.util.AttributeSet;
+
+/**
+ * Created by Anna on 5/21/17.
+ */
+
+public class GameView extends View{
+
+    public GameView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+    }
+
+
+
+}
